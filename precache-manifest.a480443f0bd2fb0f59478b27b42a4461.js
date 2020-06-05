d851bbe0c38fbbbc41d22d409ccc541e1ed9ce87
weite122 <949193531@qq.com>
2020-06-06 00:12:06 +0800

    deploy

---
+++ precache-manifest.a480443f0bd2fb0f59478b27b42a4461.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "277c883da78db87d0897",
+    "url": "/vue-bookkeeping-website/css/app.046c5e96.css"
+  },
+  {
+    "revision": "967413b6585d5124a441",
+    "url": "/vue-bookkeeping-website/css/chunk-vendors.e11c3584.css"
+  },
+  {
+    "revision": "c4d9e0c9c40835d2899ccd6832c53f3b",
+    "url": "/vue-bookkeeping-website/index.html"
+  },
+  {
+    "revision": "277c883da78db87d0897",
+    "url": "/vue-bookkeeping-website/js/app.5c61d543.js"
+  },
+  {
+    "revision": "967413b6585d5124a441",
+    "url": "/vue-bookkeeping-website/js/chunk-vendors.f792b66d.js"
+  },
+  {
+    "revision": "d1b00465103b8de3edb45d0584a5e5c4",
+    "url": "/vue-bookkeeping-website/manifest.json"
+  },
+  {
+    "revision": "04bd3b7a3f41f3e93b15e1486132cdd8",
+    "url": "/vue-bookkeeping-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-bookkeeping-website/robots.txt"
+  }
+]);
